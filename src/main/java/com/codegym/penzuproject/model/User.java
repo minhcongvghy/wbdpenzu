3f5659b9c63faf31a390917d48fc121cce6c047d
dotranghoang <dotranghoang@gmail.com>
2019-11-25 22:52:02 +0700

    json ignore role

--- src/main/java/com/codegym/penzuproject/model/User.java
+++ src/main/java/com/codegym/penzuproject/model/User.java
@@ -44,6 +44,7 @@ public class User {
     @Size(min = 6 ,max = 100)
     private String password;
 
+    @JsonIgnore
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "user_roles" ,
                 joinColumns = @JoinColumn(name = "user_id"),
